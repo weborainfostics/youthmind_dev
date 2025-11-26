bf129a0bff115f4ced563d599d4bd57f4d6c8d1f
weborainfostics <weborainfostics@gmail.com>
2025-11-26 10:08:09 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 // Use a placeholder string exactly like this
-export const GEMINI_API_KEY = "REPLACE_WITH_REAL_KEY";
+export const GEMINI_API_KEY = "AIzaSyDMRTxBQurhl19Osmzv0fucr072Jqh3juE";
+
 
 
 
